075ee083da08264abf57a61ade7bef09ca2ec02a
ShZhCh8 <1049677277@qq.com>
2023-01-07 23:49:47 +0800

    lab2 null parameter second

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -97,12 +97,14 @@ public class IntList {
      * * elements of B.  May NOT modify items of A.  Use 'new'.
      */
     public static IntList catenate(IntList A, IntList B) {
-        IntList answer = null;
-        IntList p1 = A;
-        IntList p2 = answer;
+
         if (A == null && B == null) {
             return null;
         }
+        IntList answer = new IntList();
+        IntList p1 = A;
+        IntList p2 = answer;
+
         while (p1 != null) {
             p2.first = p1.first;
             p2.rest = new IntList();
